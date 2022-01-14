282cd38bf124a576c4e5e22e8a01d6b05d6a5a81
Ilez Kurskiev <ilez.kurskiev@x5.ru>
2022-01-14 10:45:15 +0300

    Changes for env example

--- pages/api/hello.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  const { BOOM, CHAKA } = process.env
-  res.status(200).json({ name: 'John Doe', BOOM, CHAKA })
-}
--- pages/posts/first-post.js
+++
@@ -1,4 +0,0 @@
-import a from "./styles.module.css"
-export default function FirstPost() {
-    return <h1 className={a.h1} >First Post</h1>
-  }
